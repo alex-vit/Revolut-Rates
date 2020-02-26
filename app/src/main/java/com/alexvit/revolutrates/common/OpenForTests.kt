47eb8b9968f1ddc363912a091b0ce77f0948e823
alexv <a.vitjukovs@gmail.com>
2020-02-26 11:05:03 +0200

    Apply all-open correctly

---
+++ app/src/main/java/com/alexvit/revolutrates/common/OpenForTests.kt
@@ -0,0 +1,3 @@
+package com.alexvit.revolutrates.common
+
+annotation class OpenForTests
--- app/src/main/java/com/alexvit/revolutrates/rates/data/api/ApiRatesDataSource.kt
+++ app/src/main/java/com/alexvit/revolutrates/rates/data/api/ApiRatesDataSource.kt
@@ -1,11 +1,13 @@
 package com.alexvit.revolutrates.rates.data.api
 
+import com.alexvit.revolutrates.common.OpenForTests
 import io.reactivex.Flowable
 import io.reactivex.Scheduler
 import io.reactivex.annotations.SchedulerSupport
 import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
+@OpenForTests
 class ApiRatesDataSource(
     private val api: RatesApi,
     private val ioScheduler: Scheduler = Schedulers.io(),
